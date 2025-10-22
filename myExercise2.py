cbff05022837a48515bd9f199e0e148ca03c858c
Batuhan Efe Sipahican <batus-2005@hotmail.com>
2025-10-23 00:11:28 +0200

    Create myExercise2.py

---
+++ myExercise2.py
@@ -0,0 +1,13 @@
+
+sentence = input("Enter your sentence: ")
+
+
+newSentence = ""
+for ch in sentence:
+    if ch == " ":
+        newSentence += "--"
+    else:
+        newSentence += ch
+        newSentence += "-"
+
+print(f'---{newSentence}---')
