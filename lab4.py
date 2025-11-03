6050ebe31d745a382b7403b37be21417a60fc36e
Batuhan Efe Sipahican <batus-2005@hotmail.com>
2025-11-04 00:25:14 +0100

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -14,7 +14,7 @@ for i in range(2):
             print("Your card is blocked")
 
 '''
-
+''' 2
 number = int(input("Enter a number : "))
 i=2
 while(number!=0):
@@ -25,5 +25,19 @@ while(number!=0):
             break
     else:
         i+=1
-        
-print("adsa")
+'''  
+''' 3
+TICKET = 100
+buyers = 0
+
+while(TICKET != 0):
+    ticket = int(input("How many tickets do you want to buy ? "))
+    while(ticket > 4 ):
+        ticket = int(input("You can buy 4 ticket at most. Try again : "))
+    buyers += 1
+    TICKET = TICKET - ticket
+    print(f'{TICKET} tickets left')
+
+print(f'The number of buyers : {buyers}')
+'''
+
