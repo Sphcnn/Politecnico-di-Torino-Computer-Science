2693d950bd0179f388d0fa228b8f134fd98e8261
Batuhan Efe Sipahican <batus-2005@hotmail.com>
2025-11-09 22:49:34 +0100

    .

--- lab4.py
+++ lab4.py
@@ -1,1 +1,16 @@
 #Exercises of Lab4
+numbers = []
+sum_numbers = 0
+
+while True:
+    number = input("Enter a number (0 to quit): ")
+    if number == "0":
+        break   
+    numbers.append(int(number))
+    sum_numbers += int(number)
+
+if numbers:  
+    print(f"Sum of your numbers: {sum_numbers}")
+    print(f"Max and min of your numbers: {max(numbers)}, {min(numbers)}")
+else:
+    print("You didn't enter any numbers.")
